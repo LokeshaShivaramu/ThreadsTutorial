ce2d7ce78b853fab3e83a25eef8fd3779078ba15
Lokesha.Shivaramu <lokesha.shivaramu@altran.com>
2017-06-16 15:14:40 +0530

    Values changed

--- threads/src/deadlocks/Account.java
+++ threads/src/deadlocks/Account.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Account {
 	
-	private int balance = 1000;
+	private int balance = 10005;
 	
 	private static Lock lock = new ReentrantLock();
 	
